95ba861104511a10d1e81715ec0e1c68c03ac6b8
Anthony Taylor <AxTaylor@proton.me>
2025-02-07 08:41:53 -0500

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,48 @@
+
+const mobileBio = document.getElementById("biography")
+const mobileSkill = document.getElementById("skill")
+const mobileExperience = document.getElementById("experience")
+const bioBtn = document.getElementById("bio-btn")
+const skillBtn = document.getElementById("skill-btn")
+const experienceBtn = document.getElementById("experience-btn")
+const emailForm = document.getElementById("form")
+const mobileForm = document.getElementById("m-form")
+const portrait = document.getElementById("portrait")
+
+function openBio(){
+if(bioBtn){
+  return mobileBio.classList.add("active")
+}
+}
+function openSkill(){
+  if(skillBtn){
+  return mobileSkill.classList.add("active")
+}
+}
+function openExperience(){
+  if(experienceBtn){
+  return mobileExperience.classList.add("active")
+}
+}
+
+function closeMobile(){
+  mobileBio.classList.remove("active")
+  mobileSkill.classList.remove("active")
+  mobileExperience.classList.remove("active")
+}
+
+function openForm(){
+  emailForm.classList.add("active")
+  mobileForm.classList.add("active")
+}
+
+function closeForm(){
+  emailForm.classList.remove("active")
+  mobileForm.classList.remove("active")
+}
+function closeSummary(){
+close.style.display = "none"
+}
+function revealFace(){
+portrait.innerHTML = `<img src='assets/faceReveal/angry-memoji.png' class="profile-pic"/>`
+}
